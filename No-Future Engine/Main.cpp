58c37eddc2bdff0fe23d03663b8b91ad2fe7b9d9
Time7Portal <wjdals930315@gmail.com>
2022-04-24 02:32:32 +0900

    졸려서 이만..

--- No-Future Engine/Main.cpp
+++ No-Future Engine/Main.cpp
@@ -1,14 +1,15 @@
-#define GLFW_INCLUDE_VULKAN // 이렇게 정의해두면 glfw3.h 에서 vulkan.h 도 같이 불러옵니다
+#define GLFW_INCLUDE_VULKAN // 이렇게 정의해두면 glfw3.h 에서 vulkan.h 도 같이 불러옵니다.
 #include <GLFW/glfw3.h>
 
 #define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE // OpenGL과 다르게 0 ~ 1 스케일 깊이 값을 사용합니다
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE // OpenGL과 다르게 0 ~ 1 스케일 깊이 값을 사용합니다.
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
 
 #include <iostream>
 #include <stdexcept> // 예외처리
 #include <cstdlib> // EXIT_SUCCESS, EXIT_FAILURE 매크로
+#include <vector>
 
 // 윈도우 해상도
 constexpr uint32_t WIDTH = 800;
@@ -18,8 +19,8 @@ constexpr uint32_t HEIGHT = 600;
 class HelloTriangleApplication
 {
 private:
-    GLFWwindow* window; // GLFW 윈도우
-    VkInstance instance; // Vulkan 객체
+    GLFWwindow* window; // GLFW 윈도우 핸들
+    VkInstance instance; // Vulkan 객체 핸들
 
 public:
     void run()
@@ -36,50 +37,72 @@ private:
     {
         glfwInit();
 
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // GLFW 는 OpenGL context 에 기본적으로 최적화 되어 있으므로 Vulkan 용으로 사용하기 위해 GLFW_NO_API 로 명시하였습니다
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // 사용자가 윈도우 창 크기를 마음껏 조절하도록 만들려면 불칸에서 신경써야할 일이 엄청 많아지므로 고정 해상도를 사용하도록 제한하였습니다
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // GLFW 는 OpenGL context 에 기본적으로 최적화 되어 있으므로 Vulkan 용으로 사용하기 위해 GLFW_NO_API 로 명시하였습니다.
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // 사용자가 윈도우 창 크기를 마음껏 조절하도록 만들려면 불칸에서 신경써야할 일이 엄청 많아지므로 고정 해상도를 사용하도록 제한하였습니다.
 
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 
-        // 그래픽카드가 불칸 확장 기능들을 몇개 제공하는지 받아옵니다 | Gets how many Vulkan extensions graphics card can provides
+        // 인스턴스를 생성하기 전에 그래픽카드가 지원하는 불칸 확장 기능들을 확인합니다. | Gets how many Vulkan extensions graphics card can provides.
+        // 몇개의 확장 기능을 지원하는지 갯수를 우선 받은 후에
         uint32_t extensionCount = 0;
         vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
         std::cout << extensionCount << " extensions supported\n";
+        // 그 갯수 정보를 활용해서 지원 목록을 받아옵니다.
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+        // 각 VkExtensionProperties구조체에는 확장의 이름과 버전이 포함됩니다. 간단한 for 루프로 나열할 수 있습니다. (\t 는 들여쓰기)
+        std::cout << "Available extensions:\n";
+        for (const auto& extension : extensions)
+        {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
     }
 
 
     // 2. 불칸 객체 초기화 및 렌더링 준비
     void initVulkan()
     {
-        createInstance();       // 2-1. Vulkan 객체를 만듭니다
+        createInstance();       // 2-1. Vulkan 객체 만들기
     }
 
 
-    // 2-1. Vulkan 객체를 만듭니다
+    // 2-1. Vulkan 객체를 만들기
     void createInstance()
     {
+        // 앱에 대한 정보를 제공합니다. (필수는 아니지만 이 정보를 가지고 그래픽 드라이버가 최적화 하는데 사용하기도 합니다)
         VkApplicationInfo appInfo{};
-        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "Hello Triangle";
-        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.pEngineName = "No Engine";
-        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_0;
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO; // 사실상 대부분의 불칸 구조체는 sType 이라는 멤버로 가지고 형식을 파악합니다. (그냥 appInfo 타입을 보고 판단하면 되는데 굳이 이렇게 만든 이유가??)
+        appInfo.pApplicationName = "Hello Triangle"; // 앱 이름
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0); // 앱 버전
+        appInfo.pEngineName = "No-Future Engine"; // 엔진 이름
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0); // 엔진 버전
+        appInfo.apiVersion = VK_API_VERSION_1_0; // 사용할 불칸 API 버전
+        appInfo.pNext = nullptr; // 미래에 사용할지도 모르는 확장 구조체 연결 목록을 전달하려는 용도입니다.
 
+        // VkInstance는 VkInstanceCreateInfo에 의해서 생성됩니다. 구조체의 변수를 통해 어플리케이션의 정보와 어떤 Vulkan Layer 들로 Vulkan Layer Chain 을 구성할지 정의합니다. 불칸의 많은 정보는 이처럼 함수 매개변수 대신 구조체를 통해 전달되며 인스턴스 생성을 위한 충분한 정보를 제공하려면 아래 구조체를 하나 더 채워야 합니다. 다음 구조체는 사용하려는 전역 확장 및 유효성 검사 계층을 Vulkan 드라이버에 알려줍니다. 여기에서 전역이란 특정 장치가 아니라 전체 프로그램에 적용된다는 것을 의미합니다.
         VkInstanceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
 
+        // Vulkan은 플랫폼에 구애받지 않는 API이므로 윈도우 시스템과 상호작용 하려면 확장이 필요합니다. GLFW에는 필요한 확장을 반환하는 편리한 내장 함수가 있습니다.
         uint32_t glfwExtensionCount = 0;
         const char** glfwExtensions;
         glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        std::cout << "Used extensions from GLFW:\n";
+        for (int i = 0; i < glfwExtensionCount; i++)
+        {
+            std::cout << '\t' << glfwExtensions[i] << '\n';
+        }
 
+        // 활성화할 전역 유효성 검사 계층을 결정합니다.
         createInfo.enabledExtensionCount = glfwExtensionCount;
         createInfo.ppEnabledExtensionNames = glfwExtensions;
 
         createInfo.enabledLayerCount = 0;
 
-        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        // 이제 Vulkan이 인스턴스를 생성하는 데 필요한 모든 것을 지정했으며 마침내 vkCreateInstance호출을 실행할 수 있습니다. (VkInstanceCreateInfo 의 포인터, 메모리를 직접 관리하기 위한 커스텀 얼로케이터 콜백들, 불칸 인스턴스 핸들 포인터)
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+        {
             throw std::runtime_error("failed to create instance!");
         }
     }
@@ -88,9 +111,10 @@ private:
     // 3. 계속해서 매 프레임 렌더
     void mainLoop()
     {
-        // 사용자가 창을 닫을 때까지는 계속 이벤트 처리를 하면서 루프를 돕니다
+        // 사용자가 창을 닫을 때까지는 계속 이벤트 처리를 하면서 루프를 돕니다.
         while (!glfwWindowShouldClose(window))
         {
+            // GLFW 윈도우 이벤트 처리
             glfwPollEvents();
 
             // @@@ 여기서 매 프레임을 랜더링 할 예정
@@ -101,12 +125,17 @@ private:
     // 4. 프로그램 종료
     void cleanup()
     {
+        // 불칸 객체를 지웁니다. (인스턴스 핸들, 메모리를 직접 관리하기 위한 커스텀 얼로케이터 콜백)
+        vkDestroyInstance(instance, nullptr);
+
+        // GLFW 윈도우를 지웁니다.
         glfwDestroyWindow(window);
 
+        // GLFW 종료
         glfwTerminate();
 
 
-        // std::unique_ptr 나 std::shared_ptr 등을 써서 우아하게 RAII 를 처리할 수도 있지만 학습을 위해 모든 자원을 명시적으로 소멸합니다 | Possible to handle RAII more elegantly with std::unique_ptr or std::shared_ptr etc, but explicitly destroy all resources for learning now.
+        // std::unique_ptr 나 std::shared_ptr 등을 써서 우아하게 RAII 를 처리할 수도 있지만 학습을 위해 모든 자원을 명시적으로 소멸합니다. | Possible to handle RAII more elegantly with std::unique_ptr or std::shared_ptr etc, but explicitly destroy all resources for learning now.
     }
 };
 
@@ -115,7 +144,7 @@ int main()
 {
     HelloTriangleApplication app;
 
-    // HelloTriangleApplication 앱을 실행하다 오류 발생시 main 에서 에외를 받습니다 | Exception will propagate back to the main function
+    // HelloTriangleApplication 앱을 실행하다 오류 발생시 main 에서 에외를 받습니다. | Exception will propagate back to the main function.
     try
     {
         app.run();
